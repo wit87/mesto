3db05e3f6261f9cd55af0eca6e71e4342e1837f8
wit87 <wit87@list.ru>
2021-06-11 15:18:35 +0300

    version 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,41 @@
 // Делаем выборку DOM элементов
 
-const editOpenButtonElement = document.querySelector('.profile__edit-button')
-const editElement = document.querySelector('.edit-form')
-const editCloseButtonElement = document.querySelector('.edit-form__close-button')
+const popupOpenButtonElement = document.querySelector('.profile__edit-button')
+const popupElement = document.querySelector('.popup')
+const popupCloseButtonElement = document.querySelector('.popup__close-button')
 
-console.log(editElement) 
-console.log(editOpenButtonElement, editElement, editCloseButtonElement)
-
-const openEdit = function() {
-  editElement.classList.add('edit-form_is-opened')
-  console.log('Open edit clicked')
+const openpopup = function () {
+    popupElement.classList.add('popup_is-opened')
+    console.log('Open popup clicked')
 }
 
-const closeEdit = function() {
-  editElement.classList.remove('edit-form_is-opened')
+const closepopup = function () {
+    popupElement.classList.remove('popup_is-opened')
 }
 
 //Функция, которая закрывает окошко по клику на затемненную область
 
-const closeEditByClickOnOverlay = function(event) {
-  console.log(event.target, event.currentTarget)
-  if (event.target !== event.currentTarget) {
-    return
-  }
+const closepopupByClickOnOverlay = function (event) {
+    console.log(event.target, event.currentTarget)
+    if (event.target !== event.currentTarget) {
+        return
+    }
 
-  closeEdit()
+    closepopup()
 }
 
 // Регистрируем обработчики событий по клику
-editOpenButtonElement.addEventListener('click', openEdit)
-editCloseButtonElement.addEventListener('click', closeEdit)
-editElement.addEventListener('click', closeEditByClickOnOverlay)
+popupOpenButtonElement.addEventListener('click', openpopup)
+popupCloseButtonElement.addEventListener('click', closepopup)
+popupElement.addEventListener('click', closepopupByClickOnOverlay)
 
 
 // Ффункции обратного вызова
-const addEventListener = function(type, callback) {
-  console.log(type)
-  const event = {
-    target: '',
-    currentTarget: ''
-  }
-  callback(event)
+const addEventListener = function (type, callback) {
+    console.log(type)
+    const event = {
+        target: '',
+        currentTarget: ''
+    }
+    callback(event)
 }
